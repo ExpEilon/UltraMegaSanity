a6396247bce5a4779500c67a6427795c848d1c41
ExperEilon <37549495+ExpEilon@users.noreply.github.com>
2018-11-19 13:50:10 +0200

    Update getContainer function to return the path as String

--- src/main/SeeTest/BaseTest.java
+++ src/main/SeeTest/BaseTest.java
@@ -8,6 +8,7 @@ import netscape.javascript.JSObject;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import com.experitest.client.Client;
 import com.experitest.client.GridClient;
@@ -25,16 +26,17 @@ import java.util.stream.IntStream;
 
 public abstract class BaseTest {
     boolean isGrid = MyProperties.runOn.isGrid;
-    protected String projectBaseDirectory = ((MyThread)Thread.currentThread()).getDirectory();
+    protected static String projectBaseDirectory = ((MyThread)Thread.currentThread()).getDirectory();
     protected Client client = null;
     protected GridClient gridClient = null;
-    long start,end;
+    long start,end,duration;
     String query = ((MyThread)Thread.currentThread()).getQuery();
     boolean isSimulator = query.contains("emulator") ? true : false;
     Map<String, Object> launchOptionsMap;
-    SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+    static SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
     boolean createContainer = MyProperties.createContainer && MyProperties.runOn.isGrid;
-    String app ="";
+    String app;
+
     @Before
     public void setUp() {
         start = System.currentTimeMillis();
@@ -51,7 +53,7 @@ public abstract class BaseTest {
             client = gridClient.lockDeviceForExecution("TestFile", query, 30, 300000);
         else
             client.waitForDevice(query,300000);
-        if(MyProperties.createContainer && MyProperties.runOn.isGrid){
+        if(createContainer){
             launchOptionsMap = new HashMap();
             Map envVars = new HashMap();
             envVars.put("EXPERI_ENABLE_LOG_TO_CONTAINER", "true");
@@ -67,6 +69,7 @@ public abstract class BaseTest {
             client.setReporter("xml", projectBaseDirectory +"//Reporter", this.getClass().getName());
         if(MyProperties.videoRecording)
             client.startVideoRecord();
+//        client.setProperty("ios.11.use.old.monitoring.service","true");
     }
 
     @After
@@ -92,18 +95,25 @@ public abstract class BaseTest {
             getContainer(app);
         client.releaseClient();
         end = System.currentTimeMillis();
-        ((MyThread)Thread.currentThread()).setDuration((end-start)/1000);
-
+        duration = ((end-start)/1000);
+        ((MyThread)Thread.currentThread()).setDuration(duration);
+//        Assert.assertTrue("Test took too long!!\ninstead of less than : " + (MyProperties.testsRunTimes.get(this.getClass())*MyProperties.tests.length)+", it took: " + duration+"\n",
+//                (MyProperties.testsRunTimes.get(this.getClass())*MyProperties.tests.length)>duration);
     }
     protected boolean installedInstrumented(String app){
-        if(client.getInstalledApplications().contains(app)){
-            client.launch(app,true,true);
+        if(Arrays.asList(client.getInstalledApplications().split("\n")).stream().anyMatch(s -> s.equals(app+";"))){ //add ";" to the end because each row ends with it
+            try{
+                client.launch(app,true,true);
+            }catch (Exception e){
+                client.uninstall(app);
+                client.install((isGrid ? "cloud:":"") + app,true,false);
+            }
             return !client.getDeviceProperty("instrumentation.version").equals("UNKNOWN");
         }
         return false;
     }
 
-    protected int getDeviceId() throws UnirestException {
+    protected static int getDeviceId() throws UnirestException {
         HttpResponse<JsonNode> response1 = Unirest.get(MyProperties.runOn.ip+"/api/v1/devices")
                 .basicAuth(MyProperties.runOn.username, MyProperties.runOn.password)
                 .asJson();
@@ -111,16 +121,18 @@ public abstract class BaseTest {
         return IntStream.range(0,jsonArray.length()).mapToObj(i->jsonArray.getJSONObject(i)).collect(Collectors.toList()).stream()
                 .filter(j -> j.getString("udid").equals(Thread.currentThread().getName())).findFirst().map(j -> j.getInt("id")).get();
     }
-
-    protected void getContainer(String app) {
+    public static String getContainer(String app) {
+        String path = projectBaseDirectory + "//container_of_" + app + "_at_" + sdFormat.format(new Date()) + ".zip";
         try {
             HttpResponse<InputStream> response1 = Unirest.get(MyProperties.runOn.ip + "/api/v1/devices/" + getDeviceId() + "/app-container/" + app)
                     .basicAuth(MyProperties.runOn.username, MyProperties.runOn.password)
                     .asBinary();
-            FileUtils.copyInputStreamToFile(response1.getBody(), new File(projectBaseDirectory + "//container_of_" + app + "_at_" + sdFormat.format(new Date()) + ".zip"));
+            FileUtils.copyInputStreamToFile(response1.getBody(), new File(path));
         }catch (Exception e){
             e.printStackTrace();
         }
-        client.uninstall(app);
+        finally {
+            return path;
+        }
     }
 }
