0590021a2ce4053d5dca008826c27da6fde02189
ExperEilon <37549495+ExpEilon@users.noreply.github.com>
2019-05-26 19:04:35 +0300

    Paths fix

--- src/main/java/PathsMap.java
+++ src/main/java/PathsMap.java
@@ -1,8 +1,8 @@
 public class PathsMap {
-    static String UICatalog = System.getProperty("user.dir")+"\\apps\\UICatalog.ipa";
-    static String EriBank = System.getProperty("user.dir")+ "\\apps\\EriBank.ipa";
-    static String EriBankSimulator = System.getProperty("user.dir")+ "\\apps\\ExperiBankSimulator.app.zip";
-    static String PressTheDot = System.getProperty("user.dir")+"\\apps\\PressTheDot.ipa";
+    static String UICatalog = System.getProperty("user.dir")+"/apps/UICatalog.ipa";
+    static String EriBank = System.getProperty("user.dir")+ "/apps/EriBank.ipa";
+    static String EriBankSimulator = System.getProperty("user.dir")+ "/apps/ExperiBankSimulator.app.zip";
+    static String PressTheDot = System.getProperty("user.dir")+"/apps/PressTheDot.ipa";
 
     public static class Web{
         static String HibridApplication = "http://192.168.4.85:8060/html-tests/RegressionHibridApplication/HibridApplicationHtml.html";
